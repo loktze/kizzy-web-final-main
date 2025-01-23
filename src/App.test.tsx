97ba28c8d406170a0c77d55aaf8d0e71a904c8bb
Leon <leon@kizzy.io>
2025-01-24 01:43:31 +0800

    remove test and stop rerender

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Welcome to Page 1 text', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Welcome to Page 1/i);
-  expect(headingElement).toBeInTheDocument();
-});
